9d9d13936b21451a48b4bfe1fe3650065e785b18
fede <fedevargas110@gmail.com>
2019-11-13 10:52:33 -0300

    navigation created

---
+++ lib/attendees_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Attendees extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AttendeesState();
+  }
+}
+
+class _AttendeesState extends State<Attendees> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
---
+++ lib/guest_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Guest extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _GuestState();
+  }
+}
+
+class _GuestState extends State<Guest> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
